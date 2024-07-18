d0a6abf49cf08c764d83964b6f6664ba08468399
Pascal Trovatelli <34663482+ptrovatelli@users.noreply.github.com>
2024-07-18 15:25:08 +0200

    remove duplicate code

--- src/app/app-calculator/mix/mix.ts
+++ src/app/app-calculator/mix/mix.ts
@@ -6,15 +6,6 @@ export interface Mix extends ScoredObject {
     // diaas if only diaas foodItems are present in the mix
     // else pdcaas
     score_type: String,
-    histidine_score: number,
-    isoleucine_score: number,
-    leucine_score: number,
-    lysine_score: number,
-    saa_score: number,
-    aaa_score: number,
-    threonine_score: number,
-    tryptophane_score: number,
-    valine_score: number,
     protein_content: number,
     food_weight: number,
     protein_weight: number
