42730c67ae8b448920b1e4decfe3ddb234323070
Pascal Trovatelli <34663482+ptrovatelli@users.noreply.github.com>
2026-07-01 21:31:10 +0200

    replace a href by routerLink

--- src/app/carbon-footprint-info/carbon-footprint-info.component.ts
+++ src/app/carbon-footprint-info/carbon-footprint-info.component.ts
@@ -1,43 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-carbon-footprint-info',
   standalone: true,
   templateUrl: './carbon-footprint-info.component.html',
   styleUrl: './carbon-footprint-info.component.css',
+  imports: [RouterLink]
 })
 
 
 
 export class CarbonFootprintInfoComponent implements OnInit{
   ngOnInit() {
-    if (document.readyState !== 'loading') {
-      this.initCode()
-    }
-    else{
-      window.addEventListener('DOMContentLoaded', () => {
-        this.initCode()
-      });
-    }
   }
 
-  initCode(){
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-          document.querySelector(`nav li a[href="protein-carbon-footprint-information#${id}"]`)!
-            .parentElement!
-              .classList.add('active');
-        } else {
-          document.querySelector(`nav li a[href="protein-carbon-footprint-information#${id}"]`)!.parentElement!.classList.remove('active');
-        }
-      });
-    });
-  
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('h1[id],h2[id],h3[id]').forEach((section) => {
-      observer.observe(section);
-    });
-  }
 }
--- src/app/diaas-info/diaas-info.component.ts
+++ src/app/diaas-info/diaas-info.component.ts
@@ -1,43 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-diaas-info',
   standalone: true,
   templateUrl: './diaas-info.component.html',
   styleUrl: './diaas-info.component.css',
+  imports: [RouterLink]
 })
 
 
 
 export class DiaasInfoComponent implements OnInit{
   ngOnInit() {
-    if (document.readyState !== 'loading') {
-      this.initCode()
-    }
-    else{
-      window.addEventListener('DOMContentLoaded', () => {
-        this.initCode()
-      });
-    }
   }
 
-  initCode(){
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-          document.querySelector(`nav li a[href="diaas-information#${id}"]`)!
-            .parentElement!
-              .classList.add('active');
-        } else {
-          document.querySelector(`nav li a[href="diaas-information#${id}"]`)!.parentElement!.classList.remove('active');
-        }
-      });
-    });
-  
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('h1[id],h2[id],h3[id]').forEach((section) => {
-      observer.observe(section);
-    });
-  }
 }
