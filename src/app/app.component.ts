14b7b0002dc7587ce9d72740964aa3f0569dfa3a
Pascal Trovatelli <34663482+ptrovatelli@users.noreply.github.com>
2026-07-21 15:31:38 +0200

    add bootstrap for navbar and buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal} from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import {
+	NgbNav,
+	NgbNavItem,
+	NgbNavItemRole,
+	NgbNavLinkButton,
+	NgbNavLinkBase,
+	NgbNavOutlet,
+} from '@ng-bootstrap/ng-bootstrap/nav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet, RouterLink, RouterLinkActive, NgbNav, NgbNavItem, NgbNavItemRole, NgbNavLinkButton, NgbNavLinkBase, NgbNavOutlet],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Protein DIAAS calculator';
+  readonly active = signal(1);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection, importProvidersFrom } from "@angular/core";
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import {AppComponent} from './app/app.component';
 import { appConfig } from './app/app.config';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]});
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, importProvidersFrom(NgbModule)]});
 
